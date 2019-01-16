4a82cec536f059cd41d58a3d7d3d87ac4d96999b
Sam Gosling <sxg@Sams-MacBook-Pro.local>
2019-01-16 08:05:31 +0100

    Groundwork for tipUser() and telegram id storage in firebase

--- bot.js
+++ bot.js
@@ -1,14 +1,26 @@
 const location = "0xb0702df32de0371f39a98cc911a2dd69c3a13e6f";
 const json  = require("./build/contracts/ERC20d.json");
+const telegramApi = require('telegraf/telegram');
 const discordjs = require('discord.js');
 const telegraf = require('telegraf');
+
 const firebase = require('firebase');
 const Web3 = require('web3');
 
 const _web3 = new Web3(Web3.givenProvider || "https://jsonrpc.egem.io/custom");
 const _instance = new _web3.eth.Contract(json.abi, location);
+const _tg = "703021129:AAFpQDQAdFPTPmuDVRtlXLWiWgiZK8DV0xw";
 
 const _decimals = Math.pow(10,18);
+
+const _preferences = {
+  apiKey: "AIzaSyBhiCVmJR7upQQ38rt2yIt7NsqxfRTCbiU",
+  authDomain: "validitybot.firebaseapp.com",
+  databaseURL: "https://validitybot.firebaseio.com",
+  projectId: "validitybot",
+  storageBucket: "validitybot.appspot.com",
+  messagingSenderId: "73773199918"
+};
 
 initialiseDatabase = async() => {
    firebase.initializeApp(_preferences);
@@ -17,84 +29,129 @@ initialiseDatabase = async() => {
    });
  }
 
-getAccount = async(_chatid) => {
-    _chatid = "x" + _chatid;
-   return await firebase.firestore().collection(_chatid)
+getAccount = async(_username) => {
+   return await firebase.firestore().collection(_username)
    .orderBy('privateKey', 'desc').limit(1).get()
    .then(async(state) => {
-      var result;
+     var result;
       await state.forEach((asset) => {
          result = asset.data().privateKey;
       })
-    result = _web3.eth.accounts.privateKeyToAccount(result);
-    return result.address;
+    if(result == undefined){
+      return result;
+    } else {
+      result = _web3.eth.accounts.privateKeyToAccount(result);
+      return result.address;
+    }
  })
 }
 
-listAccounts = async(_chatid) => {
-    _chatid = "x" + _chatid;
-   return await firebase.firestore().collection(_chatid)
-   .orderBy('privateKey', 'desc').get()
+getID = async(_username) => {
+   return await firebase.firestore().collection(_username)
+   .orderBy('id', 'desc').limit(1).get()
    .then(async(state) => {
-      var result = [];
+     var result;
       await state.forEach((asset) => {
-         var account = _web3.eth.accounts.privateKeyToAccount(asset.data().privateKey);
-         result.push(account.address);
+         result = asset.data().id;
       })
-    return result;
+    if(result == undefined){ return result;
+    } else {
+      return result;
+    }
  })
 }
 
-createAccount = async(_chatid) => {
-  _chatid = "x" + _chatid;
-  var account = _web3.eth.accounts.create();
-  await firebase.firestore().collection(_chatid).add({
-    privateKey: account.privateKey
-  });
-  var address = await getAccount(_chatid);
-  return address;
+tipUser = async(_user1, _user2, _amount, _asset) => {
+  var reciever = await getAccount(_user2.replace('@', ''));
+  var payee = await getAccount(_user1);
+  var token = await tokenbalance(payee);
+  var gas = await gasBalance(payee);
+  _amount = parseFloat(_amount);
+  if(_asset == "VLDY" && gas != 0 && token != 0){
+    return await tokenTransfer(payee, reciever, _amount);
+  } else if(_asset == "EGEM" && gas != 0){
+    return await gasTransfer(payee, reciever, _amount);
+  } else if(payee == undefined){
+    return "Please generate an account with the command /generate."
+  } else if(reciever == undefined){
+    return "Recipent has not generated an account."
+  } else if(token == 0 && gas != 0){
+    return "No tokens available for transaction."
+  } else if(gas == 0){
+    return "No gas available for transaction."
+  }
+}
+
+createAccount = async(_username, _chatid) => {
+  if(await getAccount(_username) == undefined){
+    var account = _web3.eth.accounts.create();
+    await firebase.firestore().collection(_username).add({
+      privateKey: account.privateKey,
+      id:  _chatid
+    });
+    return `Your new account is ${account.address}`;
+  }
+  else{
+    return "You already have an account."
+  }
 }
 
 tokenbalance = async(_target) => {
   const balance = await _instance.methods.balanceOf(_target).call();
-  return parseFloat(balance/_decimals).toFixed(2) + " VLDY";
+  return parseFloat(balance/_decimals).toFixed(2);
 }
 
 gasBalance = async(_target) => {
   var balance = await _web3.eth.getBalance(_target)
-  return parseFloat(balance/_decimals).toFixed(2) + " EGEM";
+  return parseFloat(balance/_decimals).toFixed(2);
+}
+
+gasTransfer = async(_payee, _recipent, _amount) => {
+  return await _web3.eth.sendTransaction({
+    amount: parseFloat(_amount),
+    from: _payee,
+    to: _reciever,
+    gas: 2750000
+  }).send();
+}
+
+gasTransfer = async(_payee, _recipent, _amount) => {
+  return await _instance.transfer(_reciever, _amount,
+  { from: _payee, gas: 2750000 }).send();
 }
 
 initialiseDatabase();
 
 const tbot = new telegraf(_tg)
+const tapi = new telegramApi(_tg)
 
 tbot.start((ctx) => ctx.reply('Validity (VDLY) Hybrid tipbot'))
-tbot.command('bal', async(ctx) => {
-  var account = await getAccount(ctx.message.chat.id);
-  console.log(account);
+
+tbot.command('balance', async(ctx) => {
+  var account = await getAccount(ctx.message.from.username);
   var token = await tokenbalance(account);
   var gas = await gasBalance(account);
   return ctx.reply(`EGEM: ${gas}  VLDY: ${token}`);
 })
-tbot.command('acc', async(ctx) => {
-  var accounts = await listAccounts(ctx.message.chat.id);
-  console.log(accounts);
-  return ctx.reply(`Your accounts:
-    \n ${accounts[0]},
-    \n ${accounts[1]},
-    \n ${accounts[3]}`);
-})
+
 tbot.command('deposit', async(ctx) => {
-  var nuo = await getAccount(ctx.message.chat.id);
+  var nuo = await getAccount(ctx.message.from.username);
   return ctx.reply(`Your depositing address is: ${nuo}`);
 })
-tbot.command('generate', async(ctx) => {
-  var nuo = await createAccount(ctx.message.chat.id);
+
+tbot.command('yips', async(ctx) => {
+  var user = ctx.message.from.username;
+  var id =  ctx.message.chat.id;
+  var nuo = await createAccount(user, id);
   return ctx.reply(`${nuo}`);
 })
-tbot.command('tip', async(ctx) => {
-  var message = ctx.message.text.split("/tip ").pop();
-  return ctx.reply(`${message}`)
+
+tbot.command('buere', async(ctx) => {
+  var message = ctx.message.text.split("/buere ").pop();
+  var params = message.split(" ");
+  var tx = await tipUser(ctx.message.from.username,
+    params[0], params[1], params[2]);
+  return ctx.reply(`${tx}`)
 })
+
 tbot.launch()
